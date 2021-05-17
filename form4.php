8f4b3d64752717c873b0481670e1e2a106cac4ab
aduhire <71447742+aduhire@users.noreply.github.com>
2021-05-17 13:35:00 +0200

    Add files via upload

---
+++ form4.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>Homepage</title>
+        <link rel="stylesheet" href="./style.css" type="text/css">
+    </head>
+    <body>
+        <div id="headerDiv">
+            <div> 
+                <img src="./loggo.jpg" title="logo" alt="logo" height="100px" width="100px" align="left" id="logo">
+              
+            </div>
+        </div>
+
+       
+   <!-- ........................................................  -->   
+        <div id="profiles">
+            <!-- the profile photo -->
+            <p id="pic">Profile pic:</p>
+            <div id="photo"></div> 
+  <br>
+
+            <div id="line"></div>
+
+            <!-- the rank -->
+            <p id="rankLabel">User's Rank:</p>
+            <div id="rank"></div> 
+
+            <!-- User's champions -->
+            <p id="championLabel">User's champions:</p>
+            <div id="champion"></div> 
+            <div id="champion"></div> 
+            <div id="champion"></div> 
+        <br>
+        <br>
+            
+            <!-- Is that you -->
+           
+            <input   type="submit" value="yes" id="ignoreButton"> 
+            <input type="submit" value="no" id="requestButton"> 
+            <div id="rankLabel">Is That You ?</div>
+            </div> 
+       
+    </body>
+</html>
